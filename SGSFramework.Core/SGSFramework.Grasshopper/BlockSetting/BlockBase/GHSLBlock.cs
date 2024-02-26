dc01790d1c9fd19df4342562ae4c7e4b200d2b21
jy19971203 <jy.1997.12.03@gmail.com>
2024-02-26 21:59:21 +0800

    GHVersion 1.0.0

---
+++ SGSFramework.Core/SGSFramework.Grasshopper/BlockSetting/BlockBase/GHSLBlock.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Rhino.Geometry;
+using Rhino;
+using Grasshopper;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Components;
+using SGSFramework.Core.BlockSetting.BlockBase;
+
+namespace SGSFramework.Grasshopper.BlockSetting.BlockBase
+{
+    public class GHSLBlock : GH_Component
+    {
+        public override Guid ComponentGuid => throw new NotImplementedException();
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
